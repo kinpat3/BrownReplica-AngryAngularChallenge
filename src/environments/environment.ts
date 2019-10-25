6156f22af430a117d41f870e940cd3e8cb3bc7dc
DIPEN PATEL <kinpat3@yahoo.com>
2019-10-25 11:28:03 -0500

    Angular Debug Project Kinjal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase:{
-    apiKey: "",
-    authDomain: "fir-projects-75274.firebaseapp.com",
-    databaseURL: "https://fir-projects-75274.firebaseio.com",
-    projectId: "fir-projects-75274",
-    storageBucket: "fir-projects-75274.appspot.com",
-    messagingSenderId: "1090565741443",
-    appId: "1:1090565741443:web:d6b681f7ad4f60d4"
+    apiKey: "API-KEY",
+    authDomain: "angular-angry-challange-kp.firebaseapp.com",
+    databaseURL: "https://angular-angry-challange-kp.firebaseio.com",
+    projectId: "angular-angry-challange-kp",
+    storageBucket: "angular-angry-challange-kp.appspot.com",
+    messagingSenderId: "1076709443005",
+    appId: "1:1076709443005:web:a18c74debe874b7b6afed4"
   }
 };
 
